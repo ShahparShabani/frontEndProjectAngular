c8286476dafd7610470cfd4cbca0c589f473a64b
shahparshabani <shahpar.shabani@g-experts.net>
2021-10-20 17:36:58 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { AwardsComponent } from './components/awards/awards.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    CustomersComponent,
+    AwardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
